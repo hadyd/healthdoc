a9785c4431ba9f245cc90f28747a785e4d91195c
Hadyd <hadyd.arl@gmail.com>
2022-02-09 15:35:50 +0700

    setup bottom tab navigator

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,17 @@ import GetStarted from './GetStarted';
 import SignUp from './SignUp';
 import SignIn from './SignIn';
 import UploadPhoto from './UploadPhoto';
+import Doctor from './Doctor';
+import Messages from './Messages';
+import Hospitals from './Hospitals';
 
-export {Splash, GetStarted, SignUp, SignIn, UploadPhoto};
+export {
+  Splash,
+  GetStarted,
+  SignUp,
+  SignIn,
+  UploadPhoto,
+  Doctor,
+  Messages,
+  Hospitals,
+};
