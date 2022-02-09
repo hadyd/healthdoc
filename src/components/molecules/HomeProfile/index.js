7ca0fa585a2f4f62999aab8e03ecd73d650a6e01
Hadyd <hadyd.arl@gmail.com>
2022-02-09 16:52:48 +0700

    prepare base component doctor

---
+++ src/components/molecules/HomeProfile/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const HomeProfile = () => {
+  return (
+    <View>
+      <Text>Home Profile</Text>
+    </View>
+  );
+};
+
+export default HomeProfile;
+
+const styles = StyleSheet.create({});
--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {
+  DoctorCategory,
+  HomeProfile,
+  NewsItem,
+  RatedDoctor,
+} from '../../components/molecules';
 
 const Doctor = () => {
   return (
     <View>
+      <HomeProfile />
+      <Text>Mau konsultasi dengan siapa hari ini?</Text>
       <Text>Doctor Page</Text>
+      <DoctorCategory />
+      <DoctorCategory />
+      <DoctorCategory />
+      <DoctorCategory />
+      <Text>Top Rated Doctor</Text>
+      <RatedDoctor />
+      <RatedDoctor />
+      <RatedDoctor />
+      <Text>Good News</Text>
+      <NewsItem />
+      <NewsItem />
+      <NewsItem />
     </View>
   );
 };
