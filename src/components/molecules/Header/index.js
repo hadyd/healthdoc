0cc4e40b0287856904a3d12d942db96d76d13820
Hadyd <hadyd.arl@gmail.com>
2022-02-09 14:29:28 +0700

    added global fonts

--- src/components/molecules/Header/index.js
+++ src/components/molecules/Header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {Button, Gap} from '../../atoms';
-import {colors} from '../../../utils';
+import {colors, fonts} from '../../../utils';
 
 const Header = ({onPress, title}) => {
   return (
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     flex: 1,
     fontSize: 20,
-    fontFamily: 'Nunito-SemiBold',
+    fontFamily: fonts.primary[600],
     color: colors.text.primary,
   },
 });
