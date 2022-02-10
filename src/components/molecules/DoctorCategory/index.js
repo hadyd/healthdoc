6f2eb1644a01b3aa6222d940a3ae1424b6c0f480
Hadyd <hadyd.arl@gmail.com>
2022-02-10 16:41:12 +0700

    added choose doctor screen

--- src/components/molecules/DoctorCategory/index.js
+++ src/components/molecules/DoctorCategory/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
 import {ILCatObat, ILCatPsikiater, ILCatUmum} from '../../../assets';
-import {fonts} from '../../../utils';
-import {colors} from '../../../utils/colors';
+import {fonts, colors} from '../../../utils';
 
-const DoctorCategory = ({category}) => {
+const DoctorCategory = ({category, onPress}) => {
   const Icon = () => {
     if (category === 'dokter dumum') {
       return <ILCatUmum style={styles.illustration} />;
@@ -19,11 +18,11 @@ const DoctorCategory = ({category}) => {
   };
 
   return (
-    <View style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={onPress}>
       <Icon />
       <Text style={styles.label}>Saya butuh</Text>
       <Text style={styles.category}>{category}</Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
