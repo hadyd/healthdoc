90559f33cb2320ce08043894b4910ef544d21dae
Hadyd <hadyd.arl@gmail.com>
2022-04-05 10:48:32 +0700

    hide firebase API key in config

---
+++ src/config/Fire/index.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase';
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} from '@env';
+
+firebase.initializeApp({
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+});
+
+const Fire = firebase;
+
+export default Fire;
